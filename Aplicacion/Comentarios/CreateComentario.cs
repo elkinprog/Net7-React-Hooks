86d6adf4dc56d48234a163f4dbce561f37c8314c
ELKIN ALEXANDER ROJAS GALINDO <elkinprog@gmail.com>
2023-08-11 11:53:20 -0500

    Add Comentario Controller

---
+++ Aplicacion/Comentarios/CreateComentario.cs
@@ -0,0 +1,57 @@
+﻿using Aplicacion.ExcepcionMidleware;
+using Dominio.Models;
+using MediatR;
+using Persistencia.ContextConexion;
+using System.Net;
+
+namespace Aplicacion.Comentarios
+{
+    public  class CreateComentario
+    {
+    
+        public class NuevoComentario: IRequest
+        {
+            public string  Alumno          {get;set;}
+            public int     Puntaje         {get;set;}
+            public string  ComentarioTexto {get;set;}
+            public Guid    CursoId         {get;set;}
+        }
+
+
+        public class ComentarioHandler : IRequestHandler<NuevoComentario>
+        {
+            private readonly CursosOnlineContext _context;
+            public ComentarioHandler(CursosOnlineContext context)
+            {
+               this._context = context; 
+            }
+                
+            public async  Task Handle(NuevoComentario request, CancellationToken cancellationToken)
+            {
+                var comentario = new Comentario
+                {
+                    Id     = Guid.NewGuid(),
+                    Alumno = request.Alumno,
+                    Puntaje= request.Puntaje,   
+                    ComentarioTexto = request.ComentarioTexto,
+                    CursoId = request.CursoId
+                };
+                _context.Comentario.Add(comentario);
+               var resultados = await  _context.SaveChangesAsync();
+
+                if (resultados >  0)
+                {
+                    throw new GenericResponse(HttpStatusCode.OK, "Bien echo!", "se creo comentario");
+                }
+
+                throw new GenericResponse(HttpStatusCode.NotFound, "Algo sucedió!", "No se creo comentario");
+
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ WebApi/Controllers/ComentarioController.cs
@@ -0,0 +1,20 @@
+﻿using Aplicacion.Comentarios;
+using Dominio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : MiControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> CreaComentario(CreateComentario.NuevoComentario data)
+        {
+           await Mediator!.Send(data);
+           return Ok();
+        }
+    }
+}
